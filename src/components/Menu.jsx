7ba9e7655fd2a62988dca96556dcefd535f13298
Fiza Shahid <fiza34037@gmail.com>
2026-09-13 15:26:26 +0500

    Responsive Design

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -117,7 +117,7 @@ function Menu() {
         ))}
       </div>
 
-      <div className=" mt-12 grid max-w-6xl grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+      <div className="mx-auto mt-12 grid max-w-6xl grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
 
         {coffeeMenu.map((coffee,index) => (
           <div
